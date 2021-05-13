86aaba6f5e700c9881ead870df0e5cac76e583b4
OLoko64 <naldoepingo1@gmail.com>
2021-05-13 14:46:35 -0300

    Add speed multiplier

--- Main.py
+++ Main.py
@@ -2,7 +2,6 @@ import time
 import pyautogui
 from PIL import Image
 from mss import mss
-
 ##
 ##
 
@@ -10,44 +9,38 @@ from mss import mss
 
 ##
 ##
-
 time.sleep(0.5)
-# part of the screen
-# aux = True
-# count = 1
 
 coordenada1 = 550, 600
 coordenada2 = 650, 600
 coordenada3 = 760, 600
 coordenada4 = 865, 600
 
-coordenadaMouse1 = 550, 650
-coordenadaMouse2 = 650, 650
-coordenadaMouse3 = 760, 650
-coordenadaMouse4 = 865, 650
+count = 0
+
+speed = 1
+
+# coordenadaMouse1 = 550, 650
+# coordenadaMouse2 = 650, 650
+# coordenadaMouse3 = 760, 650
+# coordenadaMouse4 = 865, 650
 
 print('Started...')
 while (True):
     monitor = mss().monitors[0]
     sct_img = mss().grab(monitor)
-    # Convert to PIL/Pillow Image
     im = Image.frombytes('RGB', sct_img.size, sct_img.bgra, 'raw', 'BGRX')
 
     if (im.getpixel(coordenada1) < (54, 100, 255)):
-        # print(f'TILE FOUND at {coordenada1} or 1 TILE')
-        pyautogui.click(coordenadaMouse1)
+        pyautogui.click(550, 650 * speed)
     elif (im.getpixel(coordenada2) < (54, 100, 255)):
-        # print(f'TILE FOUND at {coordenada2} or 2 TILE')
-        pyautogui.click(coordenadaMouse2)
+        pyautogui.click(650, 650 * speed)
     elif (im.getpixel(coordenada3) < (54, 100, 255)):
-        # print(f'TILE FOUND at {coordenada3} or 3 TILE')
-        pyautogui.click(coordenadaMouse3)
+        pyautogui.click(760, 650 * speed)
     elif (im.getpixel(coordenada4) < (54, 100, 255)):
-        # print(f'TILE FOUND at {coordenada4} or 4 TILE')
-        pyautogui.click(coordenadaMouse4)
+        pyautogui.click(865, 650 * speed)
 
-    # print(color)
-    # print(f'LOOP {count}')
-    # count = count + 1
-    # aux = False
-
+    count = count + 1
+    if(count == 350):
+        speed = speed + 0.01
+    # print(speed)
